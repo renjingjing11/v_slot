58f9ec5b8c7d7fb56a7b62a85f65a0b3747cf5d5
renmingming <1634488608@qq.com>
2019-10-29 15:49:45 +0800

    作用域插槽

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="parent">
+      <Child>
+        <template v-slot:head="scope">{{scope.user.name}}</template>
+        <template #default="scope">{{scope.user.age}}</template>
+        <template v-slot:foot>底部</template>
+      </Child>
+  </div>
+</template>
+<script>
+/**
+ * 在引入子组件的时候,加入要使用子组件的插槽必须使用template标签
+ * scope:是子组件传递的data数据
+ * 具名插槽：v-slot:head="scope" 
+ * 默认插槽：#default="scope"
+ */
+import Child from './components/child'
+export default {
+  components:{
+    Child
+  }
+}
+</script>
---
+++ src/components/child.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="child">
+        <slot name="head" :user="user"></slot>
+        内容
+        <slot :user="user"></slot>
+        <slot name="foot"></slot>
+    </div>
+</template>
+<script>
+/**
+ * 作用域插槽：可以做一些逻辑操作,使用作用域插槽后，两次调用子组件可以定义不同的渲染方式。
+ * 
+ * 插槽的一些使用
+ * 1.在<slot name="head" :user="user"></slot> 相当于向父子传参数(具名插槽)
+ * 2.<slot :user="user"></slot>  相当于向父级传参数(默认插槽)
+ */
+export default {
+    data(){
+        return {
+            user:{
+                name:"任明明",
+                age:10
+            }
+        }
+    }
+}
+</script>
